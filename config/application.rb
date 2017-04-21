716306fcc6b4965c8a05de80b1d3c61a21c14847
David Janas <davidjanasr@gmail.com>
2017-04-21 15:39:16 -0400

    change bucket name

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Annotator
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-        :bucket => ENV["AWS_BUCKET"],
+        :bucket => ENV["S3_BUCKET_NAME"],
         :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
         :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
         :s3_region => ENV["AWS_REGION"]
